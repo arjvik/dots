e8f4d4d8d61e0cf72eaa853a44cf7c9c243f550d
Arjun Vikram <arjvik@gmail.com>
2020-04-17 23:15:57 -0500

    [install] update polybar install (repo, renamed deps)

--- install.sh
+++ install.sh
@@ -193,17 +193,18 @@ info_done
 info "Installing polybar"
 cd /opt
 if ! type polybar; then
-	sudo git clone --recursive https://github.com/jaagr/polybar
+	sudo git clone --recursive https://github.com/polybar/polybar
 	sudo chown -R $USER:$USER polybar
 	cd polybar
 	info "First, installing dependencies"
 	sudo apt install -y build-essential git cmake cmake-data \
 		pkg-config libcairo2-dev libxcb1-dev libxcb-util0-dev \
-		libxcb-randr0-dev libxcb-composite0-dev python-xcbgen \
+		libxcb-randr0-dev libxcb-composite0-dev \
 		xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev \
 		libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev \
 		libpulse-dev libjsoncpp-dev libmpdclient-dev \
 		libcurl4-openssl-dev libnl-genl-3-dev
+	sudo apt install -y python3-xcbgen || sudo apt install -y python-xcbgen
 	./build.sh --all-features -g -A
 else
 	info "polybar found"
