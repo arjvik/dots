2e23dd7eac5ddd02df7ae44c87bf33eb69e57788
Arjun Vikram <arjvik@gmail.com>
2020-09-14 12:29:16 -0500

    [polybar] show all bluetooth devices in module

--- polybar/.config/polybar/polybar-bluetooth.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [[ $1 == "test-connected" ]]; then
-	[[ `bluetoothctl show | grep "Powered: yes" | wc -l` -gt 0 && `bluetoothctl info | grep "Connected: yes" | wc -l` -gt 0 ]] && exit 0 || exit 1
-else
-	bluetoothctl info | grep "Alias: " | cut -d":" -f2- | cut -c2-
-fi
