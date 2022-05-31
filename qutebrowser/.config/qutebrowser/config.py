c9420d95695e4698c4cb744f2eed432847ab8150
Arjun Vikram <arjvik@gmail.com>
2022-05-31 14:26:33 -0500

    [qutebrowser] add dns-unblock and kdeconnect-share keybindings

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -24,11 +24,13 @@ c.aliases = {
     'twelveft-unblock': 'open https://12ft.io/{url}',
     'quora-unblock': 'open {url}?share',
     'easybib-unblock': 'jseval for (d of document.getElementsByTagName("div")) if (d.style["z-index"]>=9999) d.style["display"]="none";',
+    'dns-unblock': 'spawn -m bash -c \'url="$(curl -Ls -o /dev/null -w %{url_effective} --doh-url https://one.one.one.one/dns-query {url})"; echo "$url"; qutebrowser ":open $url" 2>/dev/null\'',
     'disable-cert-validation': 'set content.tls.certificate_errors load-insecurely ;; message-error "SSL certificate validation DISABLED!"',
     'youtube-speed': 'jseval document.querySelector("video, audio").playbackRate = parseFloat(prompt("Enter playback speed"))',
     'create-qr-code': 'open -t https://api.qrserver.com/v1/create-qr-code/?size=300x300&qzone=1&data={url}',
     'invert-colors': 'jseval document.documentElement.style.filter+=" invert(100%)"',
     'autofill-name': 'fake-key Vikram<tab>Arjun<tab>',
+    'kdeconnect-share': 'spawn bash -c "kdeconnect-cli -d $(kdeconnect-cli -a --id-only) --share \'{url}\'"',
     'q': 'quit',
     'w': 'session-save',
     'wq': 'quit --save'
@@ -157,10 +159,12 @@ config.bind('qbu', 'proxy-utsw')
 config.bind('qbx', 'proxy-off')
 config.bind('qc', 'classroom-switch-account')
 config.bind('qe', 'easybib-unblock')
+config.bind('qd', 'dns-unblock')
 config.bind('qv', 'disable-cert-validation')
 config.bind('qf', 'autofill-name')
 config.bind('qy', 'youtube-speed')
 config.bind('qr', 'create-qr-code')
+config.bind('qk', 'kdeconnect-share')
 
 config.unbind('<Ctrl+a>')
 config.unbind('<Ctrl+x>')
