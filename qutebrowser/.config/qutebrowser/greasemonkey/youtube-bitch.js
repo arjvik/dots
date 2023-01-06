c395c7bfabac13c897c029b3205fd26701492174
Arjun Vikram <arjvik@gmail.com>
2023-01-06 04:12:13 -0600

    [qutebrowser] stop wasting time on YouTube

---
+++ qutebrowser/.config/qutebrowser/greasemonkey/youtube-bitch.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name YouTube Bitch
+// @version 1.0.0
+// @description Stop wasting time on YouTube
+// @match *://www.youtube.com/*
+// @exclude *://music.youtube.com/*
+// ==/UserScript==
+
+alert("Get off YouTube, bitch!")
+window.location.href = "https://arjvik.github.io/tab/"
